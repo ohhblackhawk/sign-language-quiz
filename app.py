d79a14004b829647f33b96dd04fbdb8f66983954
j2-thapamagar <james2.thapamagar@live.uwe.ac.uk>
2024-01-30 22:16:36 +0000

    webcam, mediapipe

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
+import cv2
+import mediapipe as mp 
 
 app = Flask(__name__)
 
@@ -8,7 +10,35 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                #flips video so right hand displays on the right, and output is a mirror for user. 
+                image = cv2.flip(image,1)
+                image.flags.writeable = False
+                results = hands.process(image)
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                ret, buffer = cv2.imencode('.jpg', image)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
